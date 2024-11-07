427392e17bd50899ad7e88d9353968bc40c488ef
Matias Antilef <matias.romero.dv@gmail.com>
2024-11-07 20:48:15 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Esto permite que el servidor esté disponible en la red local
-    port: 3000, // Puedes cambiar el puerto si lo deseas
-  },
 });
